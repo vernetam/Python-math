0e3e26791ec5ea9e84a7285fbf5e34f3c55761d2
vernetam <60354926+vernetam@users.noreply.github.com>
2025-11-06 21:00:23 -0500

    Completed enchanced multiplication table

--- enhanced_multiplication.py
+++ enhanced_multiplication.py
@@ -1,10 +1,14 @@
 '''
 Multiplication table printer
 '''
-def multi_table(a):
-    for i in range(1, 11):
+def multi_table(a,b):
+    for i in range(1, b+1):
         print('{0} x {1} = {2}'.format(a, i, a*i))
         
 if __name__ == '__main__':
-    a = input('Enter a number: ')
-    multi_table(float(a))   
+    try:
+        a = int(input("Please enter an integer you wish to multiply: "))
+        b = int(input("Up to which multiple would you like to multiply to: "))
+        multi_table(a,b)
+    except ValueError:
+        print("Incorrect value. Ensure input is an integer.")
