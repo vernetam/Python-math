5c929429d72b5f4b1edbc95ab66ca1af9ad33160
vernetam <60354926+vernetam@users.noreply.github.com>
2026-01-06 18:15:33 -0500

    Finished implementing fraction calculator

--- fraction_calculator.py
+++ fraction_calculator.py
@@ -8,12 +8,24 @@ def add(a, b):
 def subtract(a,b):
     print('Result of subtraction: {}'.format(a-b))
 def divide(a,b):
-    print('Result of division: {}'.format(a/b))
+    try:
+        print('Result of division: {}'.format(a/b))
+    except ZeroDivisionError:
+        print('Invalid. Division by zero detected')
 def multiply(a,b):
     print('Result of multiplication: {}'.format(a*b))
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     a = Fraction(input('Enter first fraction: '))
     b = Fraction(input('Enter second fraction: '))
     op = input('Operation to perform - Add, Subtract, Divide, Multiply: ')
-    #if op == 'Add'
+    if op == 'Add': 
+        add(a,b) 
+    elif op == 'Subtract': 
+        subtract(a,b) 
+    elif op == 'Divide': 
+        divide(a,b) 
+    elif op == 'Multiply': 
+        multiply(a,b)
+    else:
+        print('You didn\'t enter a valid operation. Please try again')
