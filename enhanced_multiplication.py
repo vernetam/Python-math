086dd95a9af8b6b52484edfd76a9d3288dc05703
vernetam <60354926+vernetam@users.noreply.github.com>
2026-01-07 14:14:14 -0500

    adding exit option

--- enhanced_multiplication.py
+++ enhanced_multiplication.py
@@ -6,9 +6,13 @@ def multi_table(a,b):
         print('{0} x {1} = {2}'.format(a, i, a*i))
         
 if __name__ == '__main__':
-    try:
-        a = int(input("Please enter an integer you wish to multiply: "))
-        b = int(input("Up to which multiple would you like to multiply to: "))
-        multi_table(a,b)
-    except ValueError:
-        print("Incorrect value. Ensure input is an integer.")
+    while True:
+        try:
+            a = int(input("Please enter an integer you wish to multiply: "))
+            b = int(input("Up to which multiple would you like to multiply to: "))
+            multi_table(a,b)
+        except ValueError:
+            print("Incorrect value. Ensure input is an integer.")
+        end_program = input('Would you like to exit program? Type y to exit: ')
+        if end_program == 'y':
+            break
