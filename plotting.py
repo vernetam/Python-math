8bd9da1287a6c000d6305ca2810744338b2e039b
vernetam <60354926+vernetam@users.noreply.github.com>
2026-01-16 16:25:57 -0500

    updating data set, cleaning df for graphing

--- plotting.py
+++ plotting.py
@@ -1,6 +1,18 @@
-from pylab import plot, show
-x = [1,2,3]
-y = [2,4,6]
+import pandas as pd
+import matplotlib.pyplot as plt
 
-plot(x,y)
-show()
+climate = pd.read_csv('daily_climate.csv')
+print(climate.columns.tolist())
+
+# Rename columns to be used for graph
+climate = climate.rename(columns = {
+    'Max Temp (°C)' : 'max_temp',
+    'Date/Time' : 'date'
+})
+
+# Grab first 15 temperature data points for y-axis 
+y_axis = climate.loc[0:15, 'max_temp']
+# Grab first 15 time data points for x-axis 
+x_axis = climate.loc[0:15, 'date']
+
+#def create_graph(x,y)
